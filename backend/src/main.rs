05bbbec7439a632fb86a9c6a958d2b773a6e7f86
Leonardo Lucas de Castro Filho <contato+git@lukakuuhaku.dev>
2026-09-18 11:06:07 -0300

    adiciona pagina inicial

---
+++ backend/src/main.rs
@@ -0,0 +1,16 @@
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Server listening on http://127.0.0.1:3000");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> Html<&'static str> {
+    Html("<h1>Inicio</h1>")
+}
